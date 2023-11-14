6552e778431e42578f0cc6800fe247523843e629
juancr1010 <juancab.alumn@cescristorey.com>
2023-11-14 08:48:28 +0100

    V0.4 - He implementado la clase DatabaseConnection en el modelo, permite crear la conexión, cerrarla, insertar datos en una tabla y consultar los equipos de carreras

---
+++ formula1/src/main/java/Modelo/DatabaseConnection.java
@@ -0,0 +1,114 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author johnm
+ */
+public class DatabaseConnection {
+    Connection conn = null;
+    static {
+        try {
+            // Cargar el driver de Oracle
+            Class.forName("org.sqlite.JDBC");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection connect() throws SQLException {
+        // Cadena de conexión para Oracle
+        Connection conn = null;
+        String url = "jdbc:sqlite:D:\\Program Files\\SqliteStudio\\basededatos\\formula1.db";
+        try{
+             conn = DriverManager.getConnection(url);
+        }
+        catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+    public DatabaseConnection() {
+        
+    }
+    
+    public void insertarDatosEnTabla(String query){
+        PreparedStatement sentencia = null;
+        try{
+            conn = DatabaseConnection.connect();
+            sentencia = conn.prepareStatement(query);
+            
+            sentencia.executeUpdate();
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+        finally{
+            try{
+                if(sentencia != null){
+                    sentencia.close();
+                }
+            }
+            catch(SQLException excptn){
+                excptn.printStackTrace();
+            }
+        }
+    }
+    
+    public void consultarTablaEquiposCarreras(){
+        String cons = "SELECT * FROM equiposcarreras";
+        PreparedStatement consulta = null;
+        ResultSet resultado = null;
+        
+        try{
+            consulta = conn.prepareStatement(cons);
+            resultado = consulta.executeQuery();
+            
+            while(resultado.next()){
+                System.out.println("IDEquipo: " + resultado.getString(1));
+                System.out.println("Nombre: " + resultado.getString(2));
+            }
+            
+        }
+        catch(SQLException sqle){
+            sqle.printStackTrace();
+        }
+        finally{
+            if(consulta != null){
+                try{
+                    consulta.close();
+                    resultado.close();
+                }
+                catch(SQLException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+        
+    public void cerrarConexion(){
+        try{
+            if(conn != null){
+                conn.close();
+            }
+       }
+       catch(SQLException e){
+            e.printStackTrace();
+       }
+    }
+        
+    
+    
+    
+    
+}
