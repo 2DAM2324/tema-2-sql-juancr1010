323ba899545433ccfaac1736838f27b2094b240e
juancr1010 <juancab.alumn@cescristorey.com>
2023-11-26 11:54:41 +0100

    V0.6-Implementadas las funcionalidades para traer los equipos, pilotos e informes de la base de datos. Falta relacionarlos

--- formula1/src/main/java/Modelo/DatabaseConnection.java
+++ formula1/src/main/java/Modelo/DatabaseConnection.java
@@ -9,13 +9,24 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
+import java.util.ArrayList;
+import Modelo.EquipoCarreras;
+import Modelo.Piloto;
+import Modelo.Genera;
+import Modelo.Informe;
+import Modelo.Coche;
+import Modelo.Ingeniero;
+import java.sql.Statement;
 /**
  *
  * @author johnm
  */
 public class DatabaseConnection {
     Connection conn = null;
+    private ArrayList<EquipoCarreras> equiposBD;
+    private ArrayList<Piloto> pilotosBD;
+    private ArrayList<Informe> informesBD;
+    private ArrayList<Genera> generasBD;
     static {
         try {
             // Cargar el driver de Oracle
@@ -93,6 +104,137 @@ public class DatabaseConnection {
                 }
             }
         }
+    }
+    
+    //TODO: Debes cohesionar los informes con los pilotos, recuerda que genera guarda el informe y el piloto, no los IDs.
+    public void traerGenera(){
+        this.generasBD = new ArrayList<Genera>();
+        
+        String cons = "SELECT * FROM Genera";
+        PreparedStatement consulta = null;
+        ResultSet resultado = null;
+        
+        try{
+            consulta = conn.prepareStatement(cons);
+            resultado = consulta.executeQuery();
+            
+            while(resultado.next()){
+                
+            }
+            
+        }
+        catch(SQLException sqle){
+            sqle.printStackTrace();
+        }
+        finally{
+            if(consulta != null){
+                try{
+                    consulta.close();
+                    resultado.close();
+                }
+                catch(SQLException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    
+    public void traerInformes(){
+        this.informesBD = new ArrayList<Informe>();
+        
+        String cons = "SELECT * FROM Informe";
+        PreparedStatement consulta = null;
+        ResultSet resultado = null;
+        
+        try{
+            consulta = conn.prepareStatement(cons);
+            resultado = consulta.executeQuery();
+            
+            while(resultado.next()){
+                this.informesBD.add(new Informe(resultado.getString(1), resultado.getString(2)));
+            }
+            
+        }
+        catch(SQLException sqle){
+            sqle.printStackTrace();
+        }
+        finally{
+            if(consulta != null){
+                try{
+                    consulta.close();
+                    resultado.close();
+                }
+                catch(SQLException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    
+    public void traerPilotos(){
+        this.pilotosBD = new ArrayList<Piloto>();
+        
+        String cons = "SELECT * FROM Piloto";
+        PreparedStatement consulta = null;
+        ResultSet resultado = null;
+        
+        try{
+            consulta = conn.prepareStatement(cons);
+            resultado = consulta.executeQuery();
+            
+            while(resultado.next()){
+                this.pilotosBD.add(new Piloto(resultado.getString(1), resultado.getString(2), Integer.parseInt(resultado.getString(3))));
+            }
+            
+        }
+        catch(SQLException sqle){
+            sqle.printStackTrace();
+        }
+        finally{
+            if(consulta != null){
+                try{
+                    consulta.close();
+                    resultado.close();
+                }
+                catch(SQLException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+        
+    }
+    
+    public void traerEquiposCarreras(){
+        this.equiposBD = new ArrayList<EquipoCarreras>();
+        
+        String cons = "SELECT * FROM Piloto";
+        PreparedStatement consulta = null;
+        ResultSet resultado = null;
+        
+        try{
+            consulta = conn.prepareStatement(cons);
+            resultado = consulta.executeQuery();
+            
+            while(resultado.next()){
+                this.equiposBD.add(new EquipoCarreras(resultado.getString(1), resultado.getString(2)));
+            }
+            
+        }
+        catch(SQLException sqle){
+            sqle.printStackTrace();
+        }
+        finally{
+            if(consulta != null){
+                try{
+                    consulta.close();
+                    resultado.close();
+                }
+                catch(SQLException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+        
     }
         
     public void cerrarConexion(){
